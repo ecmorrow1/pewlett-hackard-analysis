8f9c1b11fe5e2453d7e8dd829906244d841e7834
Morrow <edward.c.morrow@gmail.com>
2022-03-16 00:55:53 -0700

    Updated .sql to have more readable text

--- schema.sql
+++ schema.sql
@@ -18,9 +18,9 @@ CREATE TABLE IF NOT EXISTS  employees (
 
 CREATE TABLE IF NOT EXISTS  dept_manager (
 dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 --    PRIMARY KEY (emp_no, dept_no)
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS  titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 --	PRIMARY KEY (emp_no, title)
 );
 
